97b1078d3931222968416e58d662ca582ba32d28
faithtanlh <faithtanlihui@gmail.com>
2022-05-31 02:14:07 +0800

    enable CORS

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
