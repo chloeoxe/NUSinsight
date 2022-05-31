0737f1bea803250f309341aee0b8668f670fe966
Chloe Ong Xi En <Chloe@Ongs-MacBook-Pro.local>
2022-05-31 21:21:28 +0800

    changed port to 8000

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 connectDB();
 
-const app = express()
-const cors = require("cors")
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
